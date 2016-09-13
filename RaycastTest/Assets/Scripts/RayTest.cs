513fea203219fc71eb5c30d3cd4a419caf728616
guitarrapc <cherishacquire@outlook.com>
2016-09-14 03:34:40 +0900

    Added RaycastTest

---
+++ RaycastTest/Assets/Scripts/RayTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class RayTest : MonoBehaviour
+{
+    void Update()
+    {
+        RaycastHit hit;
+        var downRay = new Ray(gameObject.transform.position, new Vector3(0, -1, 0));
+        if (Physics.Raycast(downRay, out hit, float.PositiveInfinity))
+        {
+            var heading = new Vector3(0, hit.transform.position.y, 0) - new Vector3(0, gameObject.transform.position.y, 0);
+            Debug.DrawRay(gameObject.transform.position, heading, Color.red);
+        }
+    }
+}
---
+++ RaycastTest/README.md
@@ -0,0 +1,16 @@
+RaycastTest
+====
+
+Thisis Unity Raycast basic sample.
+
+What for
+----
+
+Understanding what is Raycast, how to calculate and how to draw Debug.Raycast().
+
+This let me understand basic of raycast. 
+
+Unity Tutorial
+----
+
+> [Raycasting - Unity Official Tutorials](https://www.youtube.com/watch?v=EINgIoTG8D4)
