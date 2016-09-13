107edf1e34ea9c865ccbf4e5a92e61fa9e5e9170
guitarrapc <cherishacquire@outlook.com>
2016-09-14 04:17:24 +0900

    Add Rotate Sample

--- RaycastTest/Assets/Scripts/RayTest.cs
+++ RaycastTest/Assets/Scripts/RayTest.cs
@@ -5,16 +5,20 @@ using UniRx.Triggers;
 
 public class RayTest : MonoBehaviour
 {
+    public GameObject TargetGameObject;
+
     void Start()
     {
         RaycastHit hit;
 
         Observable.EveryUpdate()
-            .Select(_ => new Ray(gameObject.transform.position, new Vector3(0, -1, 0)))
+            .Select(_ => new Ray(gameObject.transform.position, TargetGameObject.transform.position - gameObject.transform.position))
             .Subscribe(ray =>
             {
+                //Debug.DrawRay(gameObject.transform.position, TargetGameObject.transform.position - gameObject.transform.position, Color.green);
+                //Debug.DrawRay(gameObject.transform.position, TargetGameObject.transform.position, Color.green);
                 if (!Physics.Raycast(ray, out hit, float.PositiveInfinity)) return;
-                var heading = new Vector3(0, hit.transform.position.y, 0) - new Vector3(0, gameObject.transform.position.y, 0);
+                var heading = hit.transform.position - gameObject.transform.position;
                 Debug.DrawRay(gameObject.transform.position, heading, Color.red);
             })
             .AddTo(this);
---
+++ RaycastTest/Assets/Scripts/RotateTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class RotateTest : MonoBehaviour
+{
+    public float Angle = 60;
+    public GameObject TargetGameObject;
+
+    private void Start()
+    {      
+        Observable.EveryUpdate()
+            .Select(_ => gameObject.transform.TransformDirection(Vector3.up)) // determine Axis Direction
+            .Subscribe(axis =>
+            {
+                var targetPosision = TargetGameObject.transform.position;
+
+                // Change this gameobjects direction to the target
+                gameObject.transform.LookAt(targetPosision);
+
+                // Rotate - Stable
+                gameObject.transform.RotateAround(targetPosision, axis, Angle * Time.deltaTime);
+
+                // Rotate - Randomize
+                // gameObject.transform.RotateAround(targetPosision, axis, Angle * Time.deltaTime * Random.Range(1, 2));
+            })
+            .AddTo(this);
+    }
+}
