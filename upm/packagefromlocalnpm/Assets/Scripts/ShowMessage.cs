34494fac54f39a0ca62365de45d6a81e044952dd
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-08-06 16:01:42 +0900

    feat: add sample scene to reference Unity Package Manager

---
+++ upm/packagefromlocalnpm/Assets/Scripts/ShowMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMessage : MonoBehaviour
+{
+    Text text;
+    int current = 1;
+
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+    }
+
+    public void ShowText()
+    {
+        var m = new Message
+        {
+            Text = "Hello",
+            Version = "1.0.3",
+        };
+        text.text = m.GetMessage() + $" ({current++})";
+    }
+}
